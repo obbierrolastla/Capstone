f833072fe7787546e6a60c833dfb35c432eb62bd
unknown <obbierrolastla@gmail.com>
2026-06-18 05:07:59 +0700

    add communication mechanism, so the rasp will get the RFID tag and compared it with the database if it correct. if it so, rasp will send the command to open the gate

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import cv2
+import serial
+import duckdb
+import re
+import string
 from datetime import datetime
 from inference_sdk import InferenceHTTPClient
 from inference_sdk.webrtc import WebcamSource, StreamConfig, VideoMetadata
@@ -12,8 +16,7 @@ client = InferenceHTTPClient.init(
 # Configure video source (webcam)
 source = WebcamSource(resolution=(1280, 720))
 
-plat_nomor = ""
-# Configure streaming options
+# # Configure streaming options
 config = StreamConfig(
     stream_output=["output_image"],  # Get video back with annotations
     data_output=["predictions","open_ai"],     # Get prediction data via datachannel
@@ -43,8 +46,34 @@ def show_frame(frame, metadata):
 def on_data(data: dict, metadata: VideoMetadata):
     if data.get("predictions").get("image").get("width") != None:  # Check if predictions are present
         print(f"Time: {datetime.now()} -> Frame {metadata.frame_id}: {data}")
-        plat_nomor = data.get("open_ai")[0]['output']
+        plat_nomor = str(data.get("open_ai")[0]['output'])
+        plat_nomor = plat_nomor[:9]
         print(f"PLAT NOMOR: {plat_nomor}")
+
+        if(len(plat_nomor) > 8):
+            check_valid(plat_nomor)
+
+def check_valid(plat_nomor):
+    print(f'plat_nomor check valid: {plat_nomor}')
+    ser = serial.Serial('COM6', 115200, timeout=2)  # Open serial port
+    RFID_tag = ser.readline().decode('utf-8').strip()
+    print(f"Received data from ESP32: {RFID_tag}")  # Print received data
+    compared_plate_number = open_database(RFID_tag)
+    print(f"Compared plate number from database: {compared_plate_number}")
+    sama = re.match(plat_nomor, compared_plate_number).group()
+    print(f"Matched plate number: {sama} |Length: {len(sama)}")
+    if(len(re.match(plat_nomor, compared_plate_number).group()) > 8):
+        print("Access granted")
+        ser.write(b'1')  # Send '1' to ESP32
+
+def open_database(RFID_Tag=None):
+    csv_file = "database.csv"
+    RFID_Tag = RFID_Tag if RFID_Tag is not None else ""
+    query = f"SELECT Plate_Number FROM database.csv WHERE RFID_Tag = '{RFID_Tag}'"
+    result = duckdb.sql(query).fetchone()
+    if result:
+        plate_number = result[0]
+        return plate_number
 
 # Run the session (blocks until closed)
 session.run()
